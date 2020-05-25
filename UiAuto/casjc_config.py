e2a652a146734dc68a02ebeeb22556a015d2b33c
root <root@localhost.localdomain>
2020-05-25 10:24:12 +0800

    update

--- UiAuto/casjc_config.py
+++ UiAuto/casjc_config.py
@@ -1,43 +1,22 @@
 """admin管理后台用户"""
 
-#后台管理员
-user1
-#销售经理账号
-user2
-#销售总监账号
-user3
-#财务经理账号
-user4
-#法务经理账号
-user5
-#运营管理员账号
-user6
-#总经理账号
-user7
+#后台管理员,销售经理账号,销售总监账号,财务经理账号,法务经理账号,运营管理员账号,总经理账号
+user1,user2,user3,user4,user5,user6,user7
 #后台通用密码
-passwd1
-passwd2
-#测试请求地址
+passwd1,passwd2
+#admin请求地址
 adminUrl
-
-#admin测试环境
-testPerson = {"adminUrl": "http://11.2.77.3:30088", "user1": "Casjc001", "user2": "lihaifeng", "user3": "wangnan", "user4": "kongshuishui", "user5": "liukaimin", "user6": "daijiwei", "user7": "tangdebing", "passwd1": "123456aA~", "passwd2": "Casjc@123"}
-
-#开发环境
-devPerson = {"adminUrl": "http://11.2.77.1:10088", "user1": "duliadmin", "user2": "duliadmin", "user3": "duliadmin", "user4": "duliadmin", "user5": "duliadmin", "user6": "duliadmin", "user7": "tangdebing", "passwd1": "Test1234!", "passwd2": "Test1234!"}
-
-"""console控制台用户"""
-
 #企业管理员账号与密码
-entuser1
-entuser2
-entpasswd
-
+entuser1,entuser2,entpasswd
 #console测试环境
-consolePerson = {"consoleUrl": "http://11.2.77.3:30086", "entuser1": "aa123", "entuser2": "yao", "entpasswd": "123456aA~"}
+consoleUrl
 
-#console开发环境
-consolePerson = {"consoleUrl": "http://11.2.77.1:10086", "entuser1": "aa123", "entuser2": "yao", "entpasswd": "123456aA~"}
+#test测试环境
+testPerson = {"admin": {"adminUrl": "http://11.2.77.3:30088", "user1": "Casjc001", "user2": "lihaifeng", "user3": "wangnan", "user4": "kongshuishui", "user5": "liukaimin", "user6": "daijiwei", "user7": "tangdebing", "passwd1": "123456aA~", "passwd2": "Casjc@123"}, "console": {"consoleUrl": "http://11.2.77.3:30086", "entuser1": "aa123", "entuser2": "yao", "entpasswd": "123456aA~"}}
+
+#dev开发环境
+devPerson = {"admin": {"adminUrl": "http://11.2.77.1:10088", "user1": "duliadmin", "user2": "duliadmin", "user3": "duliadmin", "user4": "duliadmin", "user5": "duliadmin", "user6": "duliadmin", "user7": "tangdebing", "passwd1": "Test1234!", "passwd2": "Test1234!"}, "console": {"consoleUrl": "http://11.2.77.1:10086", "entuser1": "aa123", "entuser2": "yao", "entpasswd": "123456aA~"}}
+
 
 """全局参数"""
 
