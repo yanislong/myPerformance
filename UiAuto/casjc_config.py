143040f351bb7e9f29952874ec7037e375a1b06a
root <root@localhost.localdomain>
2020-05-25 10:07:33 +0800

    update config

--- UiAuto/casjc_config.py
+++ UiAuto/casjc_config.py
@@ -1,41 +1,44 @@
 
 """admin管理后台用户"""
 
+#后台管理员
+user1
 #销售经理账号
-user_name = "Casjc001"
-
-#销售经理账号
-user_name1 = "lihaifeng"
-
+user2
 #销售总监账号
-user_name2 = "wangnan"
-
+user3
 #财务经理账号
-user_name3 = "kongshuishui"
-
+user4
 #法务经理账号
-user_name4 = "liukaimin"
-
+user5
 #运营管理员账号
-user_name5 = "daijiwei"
-
+user6
 #总经理账号
-user_name7 = "tangdebing"
-
+user7
 #后台通用密码
-user_passwd = "Casjc@123"
+passwd1
+passwd2
+#测试请求地址
+adminUrl
 
-#后台管理员密码
-user_passwd2 = "123456aA~"
+#admin测试环境
+adminPerson = {"adminUrl": "http://11.2.77.3:30088", "user1": "Casjc001", "user2": "lihaifeng", "user3": "wangnan", "user4": "kongshuishui", "user5": "liukaimin", "user6": "daijiwei", "user7": "tangdebing", "passwd1": "123456aA~", "passwd2": "Casjc@123"}
+
+#开发环境
+devPerson = {"adminUrl": "http://11.2.77.1:10088", "user1": "duliadmin", "user2": "duliadmin", "user3": "duliadmin", "user4": "duliadmin", "user5": "duliadmin", "user6": "duliadmin", "user7": "tangdebing", "passwd1": "Test1234!", "passwd2": "Test1234!"}
 
 """console控制台用户"""
 
 #企业管理员账号与密码
-user_name6 = "aa123"
-user_name8 = "yao"
+entuser1
+entuser2
+entpasswd
 
-user_passwd2 = "123456aA~"
+#console测试环境
+consolePerson = {"consoleUrl": "http://11.2.77.3:30086", "entuser1": "aa123", "entuser2": "yao", "entpasswd": "123456aA~"}
 
+#console开发环境
+consolePerson = {"consoleUrl": "http://11.2.77.1:10086", "entuser1": "aa123", "entuser2": "yao", "entpasswd": "123456aA~"}
 
 """全局参数"""
 
@@ -53,25 +56,6 @@ uppath = r"c:\usr\AutoUi.pdf"
 
 #企业用户管理云存储配额
 quota_number = "50"
-
-#admin管理后台登录地址
-
-#测试环境
-adminUrl = "http://11.2.77.3:30088"
-
-#开发环境
-#adminUrl = "http://11.2.77.1:10088"
-
-#muser = "duliadmin"
-#mpasswd = "Test1234!"
-
-#admin控制台登录地址
-
-#测试环境
-consoleUrl = "http://11.2.77.3:30086"
-
-#开发环境
-#consoleUrl = "http://11.2.77.1:10086"
 
 #申请资源类型 store存储,share共享
 restype1 = "store"
